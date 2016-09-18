1470f67a5769973303f4cb4ab5ff47fe54eb7ba4
Shafik <shafik23@gmail.com>
2016-09-17 20:01:50 -0700

    Basic functionality works - need to add global cli script.

---
+++ main.js
@@ -0,0 +1,23 @@
+let fs = require('fs');
+let ecogen = require('ecogen');
+let argv = require('yargs')
+  .usage('Usage: $0 <command> [options]')
+  .example('$0 <input_file> [output_file]', 'input_file must be valid TildeJS code; if output_file is omitted, output goes to stdin.')
+  .demand(1, 2)
+  .help('h')
+  .alias('h', 'help')
+  .argv;
+
+
+
+const inputFile = argv._[0];
+const outputFile = argv._[1];
+const outputCode = ecogen.run(fs.readFileSync(inputFile, {encoding: "utf-8"}));
+
+if (outputFile) {
+  fs.writeFileSync(outputFile, outputCode);
+  fs.closeSync(outputFile);
+} else {
+  fs.writeSync(1, outputCode);
+  fs.fsync(1, () => { process.exit(0); });
+}
